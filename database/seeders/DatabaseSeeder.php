208f19fb4b0da39ac933fef06898d65d2ceae01a
Rafa38gh <siscaro.rafael96@gmail.com>
2023-11-13 21:45:37 -0300

    seeder update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +14,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@email.com',
+            'password' => Hash::make('admin123'),
+            'status' => '1'
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create([
+            'name' => 'Commercial',
+            'email' => 'commercial@email.com',
+            'password' => Hash::make('commercial123'),
+            'status' => '2'
+        ]);
+
+        User::factory()->create([
+            'name' => 'Operational',
+            'email' => 'operational@email.com',
+            'password' => Hash::make('operational123'),
+            'status' => '3'
+        ]);
     }
 }
